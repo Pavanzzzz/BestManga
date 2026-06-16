46558e6fbb9898cdac935f64c0dfa7467121a3c8
Pavanzzzz <psai84195@gmail.com>
2026-06-16 14:29:00 +0530

    Fix API

--- src/services/app.js
+++ src/services/app.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-const API_URL = "https://bestmanga.onrender.com/mangas";
+const API =
+  "https://bestmanga.onrender.com/mangas";
 
-export const getMangas = () => axios.get(API_URL);
+export const getMangas = () =>
+  axios.get(API);
 
 export const getManga = (id) =>
-  axios.get(`${API_URL}/${id}`);
+  axios.get(`${API}/${id}`);
 
 export const addManga = (manga) =>
-  axios.post(API_URL, manga);
+  axios.post(API, manga);
 
 export const updateManga = (id, manga) =>
-  axios.put(`${API_URL}/${id}`, manga);
+  axios.put(`${API}/${id}`, manga);
 
 export const deleteManga = (id) =>
-  axios.delete(`${API_URL}/${id}`);
+  axios.delete(`${API}/${id}`);
